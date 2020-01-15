b9b5720c5d90b3bdd7a86909f7b4515e0617805a
John Reiner <johnreiner1990@gmail.com>
2020-01-15 02:39:54 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each_word(sentence)
+  returned_array = []
+  sentence_array = sentence.split(" ")
+  sentence_array.each do |word|
+    returned_array << word.reverse
+  end
+  returned_array.join(" ")
+end
+
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(" ")
+  sentence_array.collect do |word|
+    word.reverse!
+  end
+  sentence_array.join(" ")
+end
