15c640383dd0109f9a0ca43df98b0ad650b49d1b
Sam Chen <haihuan.chen@hotmail.com>
2020-01-24 19:40:21 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b} on wheat" }
+ 
+# Bueno!
+make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b} on tortilla" }
+ 
+# Lewis Carroll's Kitchen
+make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b.reverse} on #{"bread".reverse}" }
